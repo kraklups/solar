52ef2de8780f8a752621b33bb9ed84d27950c149
nexus7 <kraklups@gmail.com>
2013-01-27 16:34:31 +0100

    entity needed for userprofile added

---
+++ solar-app/src/main/java/net/kraklups/solarapp/model/module/Module.java
@@ -0,0 +1,13 @@
+package net.kraklups.solarapp.model.module;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Version;
+
+public class Module {
+
+}
