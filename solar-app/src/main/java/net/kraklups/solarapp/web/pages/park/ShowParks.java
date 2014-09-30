59c8f554aec5c9575cc7c0baab665f4935037ff9
nexus7 <kraklups@gmail.com>
2014-09-30 11:47:51 +0200

    Added functionality for Park stuff

---
+++ solar-app/src/main/java/net/kraklups/solarapp/web/pages/park/ShowParks.java
@@ -0,0 +1,9 @@
+package net.kraklups.solarapp.web.pages.park;
+
+public class ShowParks {
+
+	public ShowParks() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ solar-app/src/main/java/net/kraklups/solarapp/web/pages/park/UpdatePark.java
@@ -0,0 +1,9 @@
+package net.kraklups.solarapp.web.pages.park;
+
+public class UpdatePark {
+
+	public UpdatePark() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
