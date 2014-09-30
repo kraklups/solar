326cac20cfbbeef11c28d3da5dd6fd518bd2fae6
nexus7 <kraklups@gmail.com>
2014-09-30 12:06:11 +0200

    Added skeleton for user register view

---
+++ solar-app/src/main/java/net/kraklups/solarapp/web/pages/user/ShowRegisters.java
@@ -0,0 +1,9 @@
+package net.kraklups.solarapp.web.pages.user;
+
+public class ShowRegisters {
+
+	public ShowRegisters() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ solar-app/src/main/java/net/kraklups/solarapp/web/pages/user/UpdateRegister.java
@@ -0,0 +1,9 @@
+package net.kraklups.solarapp.web.pages.user;
+
+public class UpdateRegister {
+
+	public UpdateRegister() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
