cd038bddea0c1374f57f92c96092a7cbff1c6c8b
nexus7 <kraklups@gmail.com>
2014-10-15 19:37:25 +0200

    Added more .tml + skeleton

---
+++ solar-app/src/main/java/net/kraklups/solarapp/web/pages/company/CreateCompany.java
@@ -0,0 +1,16 @@
+package net.kraklups.solarapp.web.pages.company;
+
+import org.apache.tapestry5.annotations.SessionState;
+
+import net.kraklups.solarapp.web.services.AuthenticationPolicy;
+import net.kraklups.solarapp.web.services.AuthenticationPolicyType;
+import net.kraklups.solarapp.web.util.UserSession;
+
+@AuthenticationPolicy(AuthenticationPolicyType.AUTHENTICATED_USERS)
+public class CreateCompany {
+
+    @SessionState(create=false)
+    private UserSession userSession;
+
+
+}
