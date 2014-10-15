3de3d6069d656558ead6a2b24d3eec19f59fbc8a
nexus7 <kraklups@gmail.com>
2014-10-15 16:52:24 +0200

    Added .tml + skeleton for Element, Sensor, Datavalue, Datalogger, ...

---
+++ solar-app/src/main/java/net/kraklups/solarapp/web/pages/datalogger/CreateDatalogger.java
@@ -0,0 +1,15 @@
+package net.kraklups.solarapp.web.pages.datalogger;
+
+import org.apache.tapestry5.annotations.SessionState;
+
+import net.kraklups.solarapp.web.services.AuthenticationPolicy;
+import net.kraklups.solarapp.web.services.AuthenticationPolicyType;
+import net.kraklups.solarapp.web.util.UserSession;
+
+@AuthenticationPolicy(AuthenticationPolicyType.AUTHENTICATED_USERS)
+public class CreateDatalogger {
+
+    @SessionState(create=false)
+    private UserSession userSession;
+
+}
