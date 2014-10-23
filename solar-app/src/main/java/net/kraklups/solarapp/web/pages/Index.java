bbcfad4fa0b3e455c14e49f67c3e2699f15cc6b4
nexus7 <kraklups@gmail.com>
2014-10-23 18:54:00 +0200

    Removed Tapestry + first step .jsp layout/template

--- solar-app/src/main/java/net/kraklups/solarapp/web/pages/Index.java
+++
@@ -1,9 +0,0 @@
-package net.kraklups.solarapp.web.pages;
-
-import org.apache.tapestry5.annotations.Import;
-
-@Import(stylesheet={"context:css/bootstrap.min.css", "context:css/sb-admin.css", "context:font-awesome/css/font-awesome.css"},
-library={"context:js/Firebug/firebug.js", "context:js/jquery-1.11.0.js", "context:js/bootstrap.min.js"})
-public class Index {
-
-}
--- solar-app/src/main/java/net/kraklups/solarapp/web/services/AuthenticationPolicyType.java
+++
@@ -1,5 +0,0 @@
-package net.kraklups.solarapp.web.services;
-
-public enum AuthenticationPolicyType {
-	ALL_USERS, AUTHENTICATED_USERS, NON_AUTHENTICATED_USERS;
-}
