e4ab5b3cb186460f1b320d38dd94b4d2c47ff9e8
nexus7 <kraklups@gmail.com>
2014-11-25 12:02:33 +0100

    Fixed N:M bug between ElementPrk & Monitor

--- solar-app/src/main/java/net/kraklups/solarapp/model/elementprk/ElementPrk.java
+++ solar-app/src/main/java/net/kraklups/solarapp/model/elementprk/ElementPrk.java
@@ -1,11 +1,16 @@
 package net.kraklups.solarapp.model.elementprk;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
@@ -23,12 +28,15 @@ import com.vividsolutions.jts.geom.Point;
 
 import net.kraklups.solarapp.model.datalogger.DataLogger;
 import net.kraklups.solarapp.model.park.Park;
+import net.kraklups.solarapp.model.taskprk.Monitor;
 import net.kraklups.solarapp.model.userprofile.UserProfile;
 
 @Entity
 @Table(name="ElementPrk")
 @Inheritance(strategy=InheritanceType.JOINED)
-public abstract class ElementPrk {
+public abstract class ElementPrk implements java.io.Serializable {
+	
+	private static final long serialVersionUID = -8979274625904669832L;
 	
 	private Long elementPrkId;
 	private String elementPrkName;
@@ -38,6 +46,8 @@ public abstract class ElementPrk {
 	private UserProfile userProfile;
 	private DataLogger dataLogger;
 	private Park park;
+	
+	private Set<Monitor> monitors = new HashSet<Monitor>(0);
 	
 	@Type(type="org.hibernate.spatial.GeometryType")	
 	private Point mapElement;	
@@ -57,7 +67,20 @@ public abstract class ElementPrk {
 		this.dataLogger = dataLogger;
 		this.park = park;
 	}
-
+	
+	public ElementPrk(String elementPrkName, String elementPrkTag, Date tvi, Date lastAccess, 
+			UserProfile userProfile, DataLogger dataLogger, Park park, Set<Monitor> monitors) {
+		
+		this.elementPrkName = elementPrkName;
+		this.elementPrkTag = elementPrkTag;
+		this.tvi = tvi;
+		this.lastAccess = lastAccess;
+		this.userProfile = userProfile;
+		this.dataLogger = dataLogger;
+		this.park = park;
+		this.monitors = monitors;
+	}
+	
 	@SequenceGenerator(                                 // It only takes effect
 			name="ElementIdGenerator",                     // for databases providing
 	        sequenceName="ElementSeq", allocationSize=1)   // identifier generators.
@@ -145,6 +168,19 @@ public abstract class ElementPrk {
 		this.mapElement = mapElement;
 	}
 
+	@ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinTable(name = "monitor_elementprk", joinColumns = { 
+			@JoinColumn(name = "elementPrkId", nullable = false, updatable = false) }, 
+			inverseJoinColumns = { @JoinColumn(name = "monitorId", 
+					nullable = false, updatable = false) })
+	public Set<Monitor> getMonitors() {
+		return this.monitors;
+	}
+	
+	public void setMonitors(Set<Monitor> monitors){
+		this.monitors = monitors;	
+	}
+	
 	@Override
 	public String toString() {
 		return "ElementPrk [elementPrkId=" + elementPrkId + ", nombre=" + elementPrkName + ", tag=" + elementPrkTag +
