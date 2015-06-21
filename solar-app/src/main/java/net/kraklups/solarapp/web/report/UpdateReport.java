915a121dd8541cc15b4be670539244f37f451fe3
nexus7 <kraklups@gmail.com>
2015-06-21 19:07:07 +0200

    UpdateReport fixed

--- solar-app/src/main/java/net/kraklups/solarapp/web/report/UpdateReport.java
+++ solar-app/src/main/java/net/kraklups/solarapp/web/report/UpdateReport.java
@@ -2,6 +2,9 @@ package net.kraklups.solarapp.web.report;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
+import net.kraklups.modelutil.exceptions.DuplicateInstanceException;
 import net.kraklups.modelutil.exceptions.InstanceNotFoundException;
 import net.kraklups.solarapp.model.report.Report;
 import net.kraklups.solarapp.model.taskprkservice.TaskPrkService;
@@ -11,8 +14,11 @@ import net.kraklups.solarapp.model.userservice.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -40,17 +46,42 @@ public class UpdateReport {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/report/updateReport/{reportId}", method = RequestMethod.GET)
-	public String createReportGet(@PathVariable String reportId, Model model) 
+	public String updateReportGet(@PathVariable String reportId, Model model) 
 			throws InstanceNotFoundException {
 		
 		logger.info("Create Report page GET!");
 		
-		Report report = taskPrkService.getReportById(Long.valueOf(reportId));
+		Report report = taskPrkService.findReport(Long.valueOf(reportId));
 		
-		model.addAttribute("report", report);		
+		model.addAttribute("report", report);
 		initModelListUserProfile(model);
 				
 		return "report/updateReport";
+	}
+	
+	@RequestMapping(value = "/report/updateReport/{reportId}", method = RequestMethod.POST)
+	public String updateReportPost(@Valid @ModelAttribute("report") Report report, BindingResult result, Model model) 
+			throws DuplicateInstanceException, InstanceNotFoundException {
+				
+		if(result.hasErrors()) {
+			logger.info("Returning updateReport.jspx page");
+			
+			return "report/updateReport";
+		} else {
+			logger.info("UpdateReport done");
+			
+			model.addAttribute("report", report);
+
+			logger.info("UserSession " + SecurityContextHolder.getContext().getAuthentication().getName());
+			
+			report.setUserProfile(userService.findUserProfileByLogin(SecurityContextHolder.getContext().getAuthentication().getName()));			
+			
+			Report merda = taskPrkService.saveReport(report);
+			
+			logger.info("Update Report page POST!" + merda);
+			
+			return "redirect:/report/showReports";
+		}
 	}
 	
 	private void initModelListUserProfile(Model model) throws InstanceNotFoundException {
